956f6ef347d1f6a00f97a8761107a75bd9df4750
andi-spajk <yinandy123@gmail.com>
2023-07-10 00:33:28 -0700

    reupload old project

---
+++ psqi.py
@@ -0,0 +1,219 @@
+from datetime import datetime
+
+# field indexes
+psqi1     = 12
+psqi2     = 13
+psqi3     = 14
+psqi4     = 15
+psqi5a    = 16
+psqi5b    = 17
+psqi5c    = 18
+psqi5d    = 19
+psqi5e    = 20
+psqi5f    = 21
+psqi5g    = 22
+psqi5h    = 23
+psqi5i    = 24
+psqi5jcom = 25
+psqi5j    = 26
+psqi6     = 27
+psqi7     = 28
+psqi8     = 29
+psqi9     = 30
+
+# PSQI response scores
+psqi = {
+    # Q5,Q7,Q8
+    "Not during the past month":0,
+    "Less than once a week":1,
+    "Once or twice a week":2,
+    "Three or more times a week":3,
+
+    # Q6
+    "Very good":0,
+    "Fairly good":1,
+    "Fairly bad":2,
+    "Very bad":3,
+
+    # Q9
+    "No problem at all":0,
+    "Only a very slight problem":1,
+    "Somewhat of a problem":2,
+    "A very big problem":3
+}
+
+def calc_durat(q4):
+    tmp = float(q4)
+    if tmp >= 7:
+        return 0
+    elif 6 <= tmp < 7:
+        return 1
+    elif 5 <= tmp < 6:
+        return 2
+    elif tmp < 5:
+        return 3
+    else:
+        print(f"ERROR: calc_durat param q4 = {q4}")
+        print(f"ERROR: calc_durat var tmp = {tmp}")
+        return 2**16
+
+def calc_distb(distb_answers, q5jcom, q5j):
+    # not answered
+    if q5j == '' or q5jcom == '':
+        tmp_q5j = 0
+    # was answered, so score the question
+    else:
+        tmp_q5j = psqi[q5j]
+
+    total = 0
+    for answer in distb_answers:
+        total += psqi[answer]
+    total += tmp_q5j
+
+    if total == 0:
+        return 0
+    elif 1 <= total <= 9:
+        return 1
+    elif 9 < total <= 18:
+        return 2
+    elif total > 18:
+        return 3
+    else:
+        print(f"ERROR: calc_distb var total = {total}")
+        return 2**16
+
+def calc_laten(q2, q5a):
+    tmp_q2 = float(q2)
+    tmp_q5a = int(psqi[q5a])
+
+    if 0 <= tmp_q2 <= 15:
+        q2_new = 0
+    elif 15 < tmp_q2 <= 30:
+        q2_new = 1
+    elif 30 < tmp_q2 <= 60:
+        q2_new = 2
+    elif tmp_q2 > 60:
+        q2_new = 3
+    else:
+        print(f"ERROR: calc_laten param q2 = {q2}")
+        print(f"ERROR: calc_laten var tmp_q2 = {tmp_q2}")
+        return 2**16
+
+    tmp = q2_new + tmp_q5a
+    if tmp == 0:
+        return 0
+    elif 1 <= tmp <= 2:
+        return 1
+    elif 3 <= tmp <= 4:
+        return 2
+    elif 5 <= tmp <= 6:
+        return 3
+    else:
+        print(f"ERROR: calc_laten var tmp = {tmp}")
+        print(f"ERROR: calc_laten var tmp = q2_new + tmp_q5a = {q2_new} + {tmp_q5a}")
+        return 2**16
+
+def calc_daydys(q8, q9):
+    tmp = psqi[q8] + psqi[q9]
+    if tmp == 0:
+        return 0
+    elif 1 <= tmp <= 2:
+        return 1
+    elif 3 <= tmp <= 4:
+        return 2
+    elif 5 <= tmp <= 6:
+        return 3
+    else:
+        print(f"ERROR: calc_daydys var tmp = {tmp}")
+        print(f"ERROR: calc_daydys param q8 = {q8}")
+        print(f"ERROR: calc_daydys param q9 = {q9}")
+        return 2**16
+
+# Bless this person's heart
+# https://stackoverflow.com/a/3096984/13471272
+def to_military(time):
+    fmt = "%I:%M %p"
+    try:
+        # google sheets autoformat
+        time = datetime.strptime(time, fmt)
+    except ValueError:
+        # librecalc autoformat
+        time = datetime.strptime(time, "%I:%M:%S %p")
+    return time
+
+def calc_hse(q1, q3, q4):
+    q1_time = to_military(q1)
+    q3_time = to_military(q3)
+
+    # PSQI scoring does q1 - q3, but because of datetime's quirks,
+    # I must do it the other way around
+    tdelta = q3_time - q1_time
+    diff_sec = tdelta.seconds
+    diff_hour = abs(diff_sec) / 3600
+    if diff_hour > 24:
+        newtib = diff_hour - 24
+    elif diff_hour <= 24:
+        newtib = diff_hour
+    else:
+        print(f"ERROR: calc_hse var diff_hour = {diff_hour}")
+        return 2**16
+
+    tmphse = (float(q4) / newtib) * 100
+    if tmphse >= 85:
+        return 0
+    elif 75 <= tmphse < 85:
+        return 1
+    elif 65 <= tmphse < 75:
+        return 2
+    elif tmphse < 65:
+        return 3
+    else:
+        print(f"ERROR: calc_hse var tmphse = {tmphse}")
+        return 2**16
+
+# slpqual and meds are calculated in the same way
+def calc_slpqual_meds(q):
+    return psqi[q]
+
+def calc_psqi(response, printing):
+    # read in PSQI responses as their original strings
+    q1     = response[psqi1].strip()
+    q2     = response[psqi2].strip()
+    q3     = response[psqi3].strip()
+    q4     = response[psqi4].strip()
+    q5a    = response[psqi5a].strip()
+    q5b    = response[psqi5b].strip()
+    q5c    = response[psqi5c].strip()
+    q5d    = response[psqi5d].strip()
+    q5e    = response[psqi5e].strip()
+    q5f    = response[psqi5f].strip()
+    q5g    = response[psqi5g].strip()
+    q5h    = response[psqi5h].strip()
+    q5i    = response[psqi5i].strip()
+    q5jcom = response[psqi5jcom].strip()
+    q5j    = response[psqi5j].strip()
+    q6     = response[psqi6].strip()
+    q7     = response[psqi7].strip()
+    q8     = response[psqi8].strip()
+    q9     = response[psqi9].strip()
+    distb_answers = [q5b, q5c, q5d, q5e, q5f, q5g, q5h, q5i]
+
+    durat = calc_durat(q4)
+    distb = calc_distb(distb_answers, q5jcom, q5j)
+    laten = calc_laten(q2, q5a)
+    daydys = calc_daydys(q8, q9)
+    hse = calc_hse(q1, q3, q4)
+    slpqual = calc_slpqual_meds(q6)
+    meds = calc_slpqual_meds(q7)
+    psqi_score = durat + distb + laten + daydys + hse + slpqual + meds
+
+    if printing:
+        print(f"durat   = {durat:d}")
+        print(f"distb   = {distb:d}")
+        print(f"laten   = {laten:d}")
+        print(f"daydys  = {daydys:d}")
+        print(f"hse     = {hse:d}")
+        print(f"slpqual = {slpqual:d}")
+        print(f"meds    = {meds:d}")
+        print(f"PSQI    = {psqi_score:d}")
+    return psqi_score
---
+++ score.py
@@ -0,0 +1,84 @@
+import csv
+import math
+import sys
+
+import pss
+import psqi
+
+# indexes of field names
+timestamp = 0
+grade = 1
+exercise = 31
+screen_time = 32
+extracurr = 33
+
+# validate cmdline arguments
+# i know it's not robust, but i probably shouldn't overengineer
+# some code for an AP stats class which only I will ever run
+if len(sys.argv) < 3:
+    print("usage: $ python3 score.py input.csv output.csv [--no-print]")
+    sys.exit()
+
+printing = True
+if len(sys.argv) == 4 and sys.argv[3] == '--no-print':
+    printing = False
+
+pss_scores = []
+psqi_scores = []
+
+# main routine
+with open(sys.argv[1], "r", encoding='utf8') as fin:
+    reader = csv.reader(fin)
+    next(reader)
+
+    with open(sys.argv[2], "w", encoding='utf8') as fout:
+        writer = csv.writer(fout)
+
+        # write output field names
+        fout.write("Timestamp,Grade,PSS,PSQI,Exercise,Screen Time,Extracurriculars\n")
+
+        print("**************************************")
+        for response in reader:
+            pss_score = pss.calc_pss(response)
+            psqi_score = psqi.calc_psqi(response, printing)
+
+            pss_scores.append(pss_score)
+            psqi_scores.append(psqi_score)
+
+            # if printing:
+            print('| ' + response[timestamp], end='')
+            print(f"  PSS {pss_score:02d}", end='')
+            print(f"  PSQI {psqi_score:02d} |")
+            print("**************************************")
+
+            row = [
+                response[timestamp], response[grade],
+                pss_score, psqi_score, response[exercise],
+                response[screen_time], response[extracurr]
+                ]
+            writer.writerow(row)
+
+def mean(ls):
+    total = 0
+    for n in ls:
+        total += n
+    return total / len(ls)
+
+def std_dev(ls, mean):
+    total = 0
+    for n in ls:
+        total += ((n - mean) ** 2)
+    var = total / (len(ls) - 1)
+    return math.sqrt(var)
+
+
+print(f"\n\nSCORING COMPLETE.")
+print(f"{len(pss_scores):d} SUBJECTS ANALYZED.\n")
+
+mean_psqi = mean(psqi_scores)
+mean_pss = mean(pss_scores)
+
+print(f"MEAN    PSQI  {mean_psqi:.02f}")
+print(f"MEAN    PSS   {mean_pss:.02f}")
+print(f"STD_DEV PSQI  {std_dev(psqi_scores, mean_psqi):.02f}")
+print(f"STD_DEV PSS   {std_dev(pss_scores, mean_pss):.02f}")
